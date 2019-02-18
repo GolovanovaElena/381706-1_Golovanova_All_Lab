9370b74df19583dc4e439f3086d27dd2fc62cf6d
Елена Голованова <43187051+Lena381706-1@users.noreply.github.com>
2019-02-18 22:43:03 +0300

    Спать 2 часа сутки совсем не оч

---
+++ Matrix/Matrix_main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include "../MatrixLib/Matrix.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Creating matrix\n\n";
+	try
+	{
+		TMatrix<int> M1(3), M2(3), R(3), MCin(3);
+		int k = 1;
+		for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 3 - i; j++)
+				M1[i][j] = k++;
+
+		M2[0][0] = 1;
+		M2[0][1] = 2;
+		M2[0][2] = 3;
+		M2[1][0] = 1;
+		M2[1][1] = 2;
+		M2[2][0] = 1;
+
+		cout << "M1:" << endl << M1 << endl;
+		cout << "\nM2:" << endl << M2 << endl;
+
+		cout << "Arithmetic operations: \n\n";
+		R = M1 + M2;
+		cout << "Result M1 + M2:" << endl << R << endl;
+
+		R = M1 - M2;
+		cout << "Result M1 - M2:" << endl << R << endl;
+
+		R = M1 * M2;
+		cout << "Result M1 * M2:" << endl << R << endl;
+
+		R = M1 / M2;
+		cout << "Result M1 * M2:" << endl << R << endl;
+
+		cout << "\n\nUsing input / output streams:\n";
+		cin >> MCin;
+		cout << "\nMatrix:" << endl << MCin;
+	}
+
+	catch (TException exp)
+	{
+		exp.Print();
+	}
+
+	return 0;
+}
---
+++ Vector/Vector_main.cpp
@@ -0,0 +1,41 @@
+#include "../VectorLib/Vector.h"
+
+int main()
+{
+
+	cout << "Vectors:";
+	try
+	{
+		TVector <int> V1(3), V2(3), R(3);
+		for (int i = 0; i < 3; i++)
+		{
+			V1[i] = (i + 1 - (i + 3) * 3);
+			V2[i] = (i * 10 + 5 - i) * 2;
+		}
+		cout << "\n\nV1: " << V1 << endl;
+		cout << "V2: " << V2 << endl;
+
+		cout << "\n\nArithmetic operations:\n\n";
+
+		R = V1 + V2;
+		cout << "Result V1 + V2: " << R << endl;
+
+		R = V1 - V2;
+		cout << "Result V1 - V2: " << R << endl;
+
+		int k = V1 * V2;
+		cout << "Result V1 * V2: " << k << endl;
+
+		cout << "\n\nUsing input / output streams:\n";
+		TVector <int> V(5);
+		cin >> V;
+		cout << "\nVector entered by you: " << V << endl;
+	}
+
+	catch (TException exp)
+	{
+		exp.Print();
+	}
+
+	return 0;
+}
